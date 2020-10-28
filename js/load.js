389d9957ee95ae4b3f47fc73b2e2178e069c2e5f
peterfeld <89661123000@mail.ru>
2020-10-28 13:25:20 +0300

    Правит шаблонные строки, убирает лишнии альтернативные ветки

--- js/load.js
+++ js/load.js
@@ -13,15 +13,15 @@
     xhr.addEventListener(`load`, function () {
       if (xhr.status === Code.OK) {
         onSuccess(xhr.response);
-      } else {
-        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
+        return;
       }
+      onError(`Статус ответа: ${xhr.status} ${xhr.statusText}`);
     });
     xhr.addEventListener(`error`, function () {
       onError(`Произошла ошибка соединения`);
     });
     xhr.addEventListener(`timeout`, function () {
-      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+      onError(`Запрос не успел выполниться за ${xhr.timeout} мс`);
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
